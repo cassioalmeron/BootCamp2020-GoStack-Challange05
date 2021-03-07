1cd810f6b793080bb020f42b62d76f6759f28933
Cassio Almeron <cassioalmeron@gmail.com>
2021-03-07 15:54:23 -0300

    Conclusion Challange 05

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface RequestDto {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,14 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: RequestDto): Transaction {
+    if (type === 'outcome') {
+      const balance = this.transactionsRepository.getBalance();
+      if (value > balance.total)
+        throw Error('There are not balance to this outcame transaction!');
+    }
+
+    return this.transactionsRepository.create({ title, value, type });
   }
 }
 
